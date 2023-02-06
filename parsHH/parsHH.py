ffc30a827bbb1e9a5f75bb050653d7c9bcb315e4
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-02-06 17:09:12 +0300

    pars

---
+++ parsHH/parsHH.py
@@ -0,0 +1,31 @@
+import requests
+
+def fetch(url, params):
+    headers = params["headers"]
+    body = params['body']
+    if params["method"] == "GET":
+        return requests.get(url, headers=headers)
+    if params["method"] == 'POST':
+        return requests.post(url, headers=headers, data=body)
+
+professions = fetch("https://api.hh.ru/specializations", {
+  "headers": {
+    "User-Agent": "MyApp/1.0"
+  },
+  "body": None,
+  "method": "GET"
+})
+
+id_list = []
+name_list = []
+
+for i in professions.json():
+    for id in i['specializations']:
+        id_list.append(id["id"])
+
+for i in professions.json():
+    for id in i['specializations']:
+        name_list.append(id["name"])
+
+diction = dict(zip(name_list, id_list))
+print(diction)
