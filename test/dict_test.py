8cc9f48dd7802c137c4932174238e394caea9fa4
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-02-06 18:33:22 +0300

    test

--- test/dict_test.py
+++ test/dict_test.py
@@ -1,5 +1,10 @@
 import unittest
 from dict import *
+
 class json_search_test(unittest.TestCase):
     def test_id(self):
         self.assertTrue([]!=fetch(f, key1))
+
+    def test_id_in_dict(self):
+        a = fetch(f, key2)
+        self.assertTrue(a == "Оптимизация сайта (SEO)")
