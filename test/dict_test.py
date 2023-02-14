86d9d82c019f0cf0e09d2bdfac546931a177ecb9
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-02-14 12:08:30 +0500

    test

--- test/dict_test.py
+++ test/dict_test.py
@@ -8,3 +8,6 @@ class json_search_test(unittest.TestCase):
     def test_id_in_dict(self):
         a = fetch(f, key2)
         self.assertTrue(a == "Оптимизация сайта (SEO)")
+
+    def test_asseq(self):
+        self.assertEqual(type(fetch(f, key1)),str)
