c9cf5e600f5d13a982bafd593bc12bbbe42ecbb9
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-02-19 15:37:03 +0300

    test

--- test/dict_test.py
+++ test/dict_test.py
@@ -3,11 +3,9 @@ from dict import *
 
 class json_search_test(unittest.TestCase):
     def test_id(self):
-        self.assertTrue([]!=fetch(f, key1))
-
-    def test_id_in_dict(self):
-        a = fetch(f, key2)
-        self.assertTrue(a == "Оптимизация сайта (SEO)")
-
-    def test_asseq(self):
-        self.assertEqual(type(fetch(f, key1)),str)
+        self.assertTrue([]!=fetch(f))
+    def test1(self):
+        for i in list1:
+            a = i
+            b = fetch(f)[a]
+            self.assertEqual(type(b), str)
