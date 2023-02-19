375c90e462145958e8b75b326949879a2e1bbb83
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-02-19 15:42:11 +0300

    test

--- test/dict_test.py
+++ test/dict_test.py
@@ -4,8 +4,11 @@ from dict import *
 class json_search_test(unittest.TestCase):
     def test_id(self):
         self.assertTrue([]!=fetch(f))
-    def test1(self):
+    def test_test1(self):
         for i in list1:
             a = i
             b = fetch(f)[a]
             self.assertEqual(type(b), str)
+
+    def test_test2(self):
+        self.assertEqual(dict1, fetch(f))
