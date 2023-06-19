94d6b1c723df6a2e98b4f9205f4f1f64dc31669c
RUZEV <121379186+RUZEV@users.noreply.github.com>
2023-06-19 16:24:28 +0500

    test

--- parsHH/parsHH.py
+++ parsHH/parsHH.py
@@ -4,17 +4,17 @@ def get_all_dict(url, params):
     headers = params["headers"]
     body = params['body']
     if params["method"] == "GET":
-        return requests.get(url, headers=headers)
+        return create_dict(requests.get(url, headers=headers))
     if params["method"] == 'POST':
-        return requests.post(url, headers=headers, data=body)
+        return create_dict(requests.post(url, headers=headers, data=body))
 def create_dict(diction):
-    professions = get_all_dict("https://api.hh.ru/specializations", {
-        "headers": {
-            "User-Agent": "MyApp/1.0"
-        },
-        "body": None,
-        "method": "GET"
-    })
+    professions = diction  #get_all_dict("https://api.hh.ru/specializations", {
+    #     "headers": {
+    #         "User-Agent": "MyApp/1.0"
+    #     },
+    #     "body": None,
+    #     "method": "GET"
+    # })
     id_list = []
     name_list = []
     for i in professions.json():
--- test/dict_test.py
+++ test/dict_test.py
@@ -1,9 +1,9 @@
 import unittest
 from unittest.mock import patch
 import requests
+import parsHH  # Импорт вашего модуля parsHH
 
 class TestDictProfession(unittest.TestCase):
-
     @patch('requests.get')
     def test_create_dict(self, mock_get):
         mock_response = [{"specializations": [{"id": 1, "name": "Profession 1"}, {"id": 2, "name": "Profession 2"}]}]
@@ -11,7 +11,7 @@ class TestDictProfession(unittest.TestCase):
 
         with patch('requests.get') as mock_get:
             mock_get.return_value.json.return_value = mock_response
-            result = create_dict({})
+            result = parsHH.create_dict({})  # Обращение к функции create_dict из parsHH модуля
             self.assertEqual(result, expected_dict)
 
 
